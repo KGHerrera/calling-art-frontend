e0c8254fe2d32733ff093bc8576ac115663b4bfb
Enrique Herrera <bycrashdash@gmail.com>
2022-07-08 18:44:24 -0500

    feat(styles): se agrego el archivo Footer.css

--- calling-art-frontend/src/App.js
+++ calling-art-frontend/src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
     <NavBar />
+    <div className="content"></div>
     <Footer />
     </>
   );
--- calling-art-frontend/src/components/Footer.js
+++ calling-art-frontend/src/components/Footer.js
@@ -1,14 +1,15 @@
 import {AiOutlineYoutube, AiFillTwitterCircle, AiOutlineGithub, AiOutlineInstagram} from 'react-icons/ai';
+import '../styles/Footer.css';
 
 export default function Footer( props ){
     return (
         <div className="footer-conteiner">
             <div className="follow-conteiner">
                 <p>follow us</p>
-                <AiOutlineYoutube />
-                <AiFillTwitterCircle />
-                <AiOutlineGithub />
-                <AiOutlineInstagram />
+                <AiOutlineYoutube className='follow-icon' />
+                <AiFillTwitterCircle className='follow-icon' />
+                <AiOutlineGithub className='follow-icon' />
+                <AiOutlineInstagram className='follow-icon' />
             </div>
             <div className="contact-us-conteiner">
                 <p>contact us calling-art@gmail.com</p>
